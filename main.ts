bd26223edb69ccc97bebd212dc9e7e48efbf5db1
SequentialEntropy <asahi79289@gmail.com>
2025-06-07 22:40:24 +0100

    Copy ExperimentalMinecartController to CustomMinecartController

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ import { Toolbar } from "./components/Toolbar.js"
 import { Vec3d } from "./world/Vec3d.js"
 import { World } from "./world/World.js"
 import { ExperimentalMinecartController } from "./world/ExperimentalMinecartController.js"
+import { CustomMinecartController } from "./world/CustomMinecartController.js"
 
 const WORLD = new World()
 const TPS = 20
@@ -22,10 +23,10 @@ async function autoPreset() {
     const json = await response.json()
     WORLD.import(json)
     WORLD.entities = [
-        new AbstractMinecartEntity(Vec3d.fromXYZ(-185.5, 0, 15.5), Vec3d.fromXYZ(1.6, 0, 0), 0, WORLD, ExperimentalMinecartController),
-        new AbstractMinecartEntity(Vec3d.fromXYZ(-105.5, 0, 19.5), Vec3d.fromXYZ(1.3, 0, 0), 0, WORLD, ExperimentalMinecartController),
-        new AbstractMinecartEntity(Vec3d.fromXYZ(150.5, 0, 5.5),   Vec3d.fromXYZ(1.7, 0, 0), 0, WORLD, ExperimentalMinecartController),
-        new AbstractMinecartEntity(Vec3d.fromXYZ(200.5, 0, 19.5),  Vec3d.fromXYZ(1.4, 0, 0), 0, WORLD, ExperimentalMinecartController),
+        new AbstractMinecartEntity(Vec3d.fromXYZ(-185.5, 0, 15.5), Vec3d.fromXYZ(1.6, 0, 0), 0, WORLD, CustomMinecartController),
+        new AbstractMinecartEntity(Vec3d.fromXYZ(-105.5, 0, 19.5), Vec3d.fromXYZ(1.3, 0, 0), 0, WORLD, CustomMinecartController),
+        new AbstractMinecartEntity(Vec3d.fromXYZ(150.5, 0, 5.5),   Vec3d.fromXYZ(1.7, 0, 0), 0, WORLD, CustomMinecartController),
+        new AbstractMinecartEntity(Vec3d.fromXYZ(200.5, 0, 19.5),  Vec3d.fromXYZ(1.4, 0, 0), 0, WORLD, CustomMinecartController),
     ]
 }
 
