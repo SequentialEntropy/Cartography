4a3b8d78ac696fd1b9094b75719de293cd1d08f7
SequentialEntropy <asahi79289@gmail.com>
2025-06-07 17:40:04 +0100

    Ignore all compiled js files

--- js/components/BlockState.js
+++
@@ -1,9 +0,0 @@
-export class BlockState {
-    constructor(state) {
-        this.state = state;
-    }
-    get() {
-        return this.state;
-    }
-}
-BlockState.AIR = new BlockState(null);
--- js/world/MathHelper.js
+++
@@ -1,6 +0,0 @@
-export class MathHelper {
-    static clamp(value, min, max) {
-        return Math.min(Math.max(value, min), max);
-    }
-}
-MathHelper.SQUARE_ROOT_OF_TWO = Math.sqrt(2.0);
